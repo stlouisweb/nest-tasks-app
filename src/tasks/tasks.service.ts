4a1001d8a5f64c1de416baa3f9f65a8a8e062685
Jeremy Plack <stlouisweb@gmail.com>
2018-11-18 16:01:40 -0600

    adds TasksService Constructor.

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,11 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Task } from './interfaces/task.interface';
 
 @Injectable()
 export class TasksService {
+  constructor(@InjectModel('Task') private readonly taskModel: Model<Task>) {}
   async create() {}
   async update() {}
   async remove() {}
