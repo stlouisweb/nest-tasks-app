ed3a569edac7e69ce9e8b5b0e11765882040c1ff
Jeremy Plack <stlouisweb@gmail.com>
2018-11-18 21:01:14 -0600

    adds timestamps to mongo schema.

--- src/tasks/schemas/task.schema.ts
+++ src/tasks/schemas/task.schema.ts
@@ -1,8 +1,11 @@
 import * as mongoose from 'mongoose';
 
-export const TaskSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  order: Number,
-  complete: Boolean,
-});
+export const TaskSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    order: Number,
+    complete: Boolean,
+  },
+  { timestamps: true },
+);
